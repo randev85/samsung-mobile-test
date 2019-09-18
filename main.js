908035c1f1dd3c355ca2163c575bf83b78c9904c
Raj Randev <40559725+randev85@users.noreply.github.com>
2019-09-18 16:48:13 +0100

    2

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
+$(document).on('click', '#price ul li', function() {
+    if ( $(this).hasClass('active') ) {
+        $(this).removeClass('active');
+    } else {
+        $('#price ul li').removeClass('active');
+        $(this).addClass('active');    
+    }
+});
+var selectedCount = $('#spec ul li').siblings().filter('.active').length;
 
-function highlight() {
-    document.getElementsByClassName('btn').style.color = 'red'
-}
+$(document).on('click', '#spec ul li', function() {
+    
+    if (selectedCount < 3 || $(this).hasClass('active')) {
+      $(this).toggleClass('active');
+    } else {
+      console.log('only 3 can be selected');
+    }   
+  });
 
+  if ($('#price ul li.active').length > 0 && $('#spec ul li.active').length > 1) {
+    $("footer").css("display", "block");
+  } else {
+    $("footer").css("display", "none");
+  }
